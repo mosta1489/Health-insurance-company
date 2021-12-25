0338fe0f529e70099910507604d73e8d3faaaf9d
Mostafa Ahmed <79556633+mosta1489@users.noreply.github.com>
2021-12-26 00:31:48 +0200

    adding method to create customer and hospital

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ def select(quary):
     cr = db.cursor()
     cr.execute(quary)
     data = cr.fetchall()
-    cr.close()
     return data
 
 
@@ -40,28 +39,103 @@ def login():
             return redirect(url_for('admin'))
 
         else:
-            # return jsonify({'data_user': data})
-            return redirect(f'/customer?user={user}&password={password}')
+            '''
+            test_pass = select(
+                f'SELECT password FROM customers WHERE user_name="{user}";')
+            if password == test_pass:
+                return redirect(f'/customer?user={user}')
+            '''
+            return redirect(f'/customer?user={user}')
+
+    return redirect('/')
 
 
 @app.route('/insert_customer', methods=['POST'])
 def insert_customer():
+    if request.method == 'POST':
+        user_name = request.form.get('user_name')
+        customer_name = request.form.get('customer_name')
+        phone = request.form.get('phone')
+        password = request.form.get('password')
+        # plan_type = request.form.get('plan_type')
+        plan_type = 'Basic'
 
+        '''
+        
+        cr = db.cursor()
+
+        # check if new user_name is already exist or not
+        cr.execute(f'SELECT user_name FROM customer WHERE user_name="{user_name}";')
+        users = cr.fetchall()
+        if users:  # if returned list has values that mean user already exist
+            return redirect('/')
+
+        # Insert Into customer table
+        cr.execute(f"INSERT INTO customers(user_name,name ,phone, password)"
+                   f" VALUES('{user_name}', '{customer_name}', '{phone}', '{password}' );")
+
+        # Insert Into Plan Table
+        cr.execute(f"INSERT INTO Plan(type, owner) VALUES('{plan_type}', '{user_name}');")
+
+        # get id of this new plan
+        cr.execute(f"SELECT plan_id FROM plan WHERE owner='{user_name}'")
+        plan_id = cr.fetchone()
+
+        # Insert Into Dependent Table
+        cr.execute(f"INSERT INTO Plan(name, plan, relation) VALUES('{user_name}', '{plan_id}', {'owner'} );")
+
+        cr.close()
+        
+        '''
     return redirect('/')
 
 
-@app.route('/insert_hospital', methods=['POST'])
+@app.route('/insert_hospital', methods=['POST', 'GET'])
 def insert_hospital():
+    if request.method == 'POST':
+        hospital_name = request.form.get('hospital_name')
+        location = request.form.get('location')
+        phone = request.form.get('phone')
+        plan_type_list = request.form.getlist('plan_type')
+
+        '''
+        
+        cr = db.cursor()
+
+        # check if new hospital is already exist or not
+        cr.execute(f'SELECT user_name FROM customer WHERE user_name="{hospital_name}";')
+        users = cr.fetchall()
+        if users:  # if returned list has values that mean user already exist
+            return redirect('/')
+
+        # insert hospital data in hospital table
+        cr.execute(f"INSERT INTO hospital (name, location, phone)"
+                   f" values('{hospital_name}', '{location}', '{phone}');")
+
+        # get hospital_id of new hospital
+        cr.execute(f"SELECT id FROM hospital WHERE name='{hospital_name}';")
+        hospital_id = cr.fetchone()
+
+        # insert data to cover table
+        for type in plan_type_list:
+            cr.execute(f"INSERT INTO cover (hospital, plan)"
+                       f" values('{hospital_id}', '{type}');")
+        
+        cr.close()
+        
+        '''
+
+        return render_template('admin.html', plan_type=plan_type_list)
 
     return redirect('/')
 
 
-@ app.route('/admin')
+@app.route('/admin')
 def admin():
     return render_template('admin.html')
 
 
-@ app.route('/customer', methods=['POST', 'GIT'])
+@app.route('/customer', methods=["GET"])
 def customer():
     user = request.args.get('user')
     password = request.args.get('password')
